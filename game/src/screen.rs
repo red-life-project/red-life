0a94c146aa1fe07378c17209dc2cd85e0ee96c6b
Bene <37740907+Nereuxofficial@users.noreply.github.com>
2022-10-29 17:52:30 +0200

    Fixed ci and extended screen.rs

--- game/src/screen.rs
+++ game/src/screen.rs
@@ -1,9 +1,13 @@
 use std::fmt::Debug;
 use crate::error;
-
 type RedResult<T = ()> = Result<T, error::RedError>;
 /// A screen is every drawable object in the game, so the main menu is a screen too
 pub trait Screen: Debug {
     fn update(&mut self) -> RedResult;
     fn draw(&self) -> RedResult;
+}
+
+/// Now we can use this trait to make for example a scene
+pub struct Screenstack {
+    screens: Vec<Box<dyn Screen>>,
 }
