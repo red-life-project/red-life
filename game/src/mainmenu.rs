1089feb18356f9c11a64afe91ac73d2a6cedc45e
mfloto <60036186+mfloto@users.noreply.github.com>
2022-11-04 19:47:40 +0100

    First implementation of button. Added: -attributes to button struct: pos, size, color -made img attribute optional -function to generate mesh for button -methode on button struct for pressed action (to handle visual changes) -handle mouse click event

--- game/src/mainmenu.rs
+++ game/src/mainmenu.rs
@@ -1,8 +1,10 @@
 use std::sync::mpsc::{channel, Receiver, Sender};
 use crate::screen::{Screen, StackCommand};
 use crate::RedResult;
-use ggez::{graphics, Context};
+use ggez::{graphics, Context, GameResult};
 use ggez::event::MouseButton;
+use ggez::glam::Vec2;
+use ggez::graphics::Color;
 use crate::gamestate::GameState;
 use crate::utils::get_scale;
 
@@ -16,10 +18,23 @@ pub enum Message {
 #[derive(Debug)]
 struct Button<Message: Clone> {
     text: String,
-    img: graphics::Image,
-    message: Message,
-    sender: Sender<Message>
+    img: Option<graphics::Image>,
+    message: self::Message,
+    sender: Option<Sender<Message>>,
+    // position of button on screen: (x, y)
+    pos: Vec2,
+    // size of button: (width, height)
+    size: Vec2,
+    color: Color,
 }
+
+impl Button<Message> {
+    fn pressed(&mut self) {
+        self.color = Color::GREEN;
+    }
+}
+
+
 #[derive(Debug)]
 pub struct MainMenu<Message: Clone> {
     buttons: Vec<Button<Message>>,
@@ -27,11 +42,38 @@ pub struct MainMenu<Message: Clone> {
     sender: Sender<Message>
 }
 
+//TODO: pos in struct for buttons
+
+fn draw_button(ctx: &mut Context, btn: &Button<Message>) -> GameResult<graphics::Mesh> {
+    let mb = &mut graphics::MeshBuilder::new();
+
+    mb.rectangle(
+        graphics::DrawMode::fill(),
+        graphics::Rect::new(btn.pos[0], btn.pos[1], btn.size[0], btn.size[1]),
+        btn.color,
+    )?;
+
+    Ok(graphics::Mesh::from_data(ctx, mb.build()))
+}
+
 impl<Message: Clone> Default for MainMenu<Message> {
     fn default() -> Self {
+
+        let default_button = Button {
+            text: "Default Button".to_string(),
+            img: None,
+            message: self::Message::NewGame,
+            sender: None,
+            pos: Vec2::new(200.0, 100.0),
+            size: Vec2::new(350.0, 100.0),
+            color: Color::CYAN
+        };
+
         let (sender, receiver) = channel();
         Self{
-            buttons: vec![],
+            buttons: vec![
+                default_button
+            ],
             receiver,
             sender
         }
@@ -42,7 +84,18 @@ impl Screen for MainMenu<Message> {
     fn update(&mut self, ctx: &mut Context) -> RedResult<StackCommand> {
         // TODO: Replace with if buttons are clicked
         if ctx.mouse.button_pressed(MouseButton::Left) {
-            return Ok(StackCommand::Push(Box::new(GameState::default())));
+            println!("Left mouse button pressed");
+            let current_position = ctx.mouse.position();
+            for mut btn in &mut self.buttons {
+                if current_position.x > btn.pos[0] && current_position.x < btn.pos[0] + btn.size[0] {
+                    if current_position.y > btn.pos[1] && current_position.y < btn.pos[1] + btn.size[1] {
+                        btn.pressed();
+                        println!("Button clicked");
+                        //self.sender.send(btn.message.clone()).unwrap();
+                    }
+                }
+            }
+            //return Ok(StackCommand::Push(Box::new(GameState::default())));
         }
         Ok(StackCommand::None)
     }
@@ -54,7 +107,17 @@ impl Screen for MainMenu<Message> {
         let background =
             graphics::Image::from_bytes(ctx, include_bytes!("../../assets/mainmenu.png"))?;
         canvas.draw(&background, graphics::DrawParam::default().scale(scale));
+        let mut btn_meshes = Vec::new();
+
+        for btn in self.buttons.iter() {
+            btn_meshes.push(draw_button(ctx, btn)?);
+        }
+        for mesh in btn_meshes {
+            canvas.draw(&mesh, graphics::DrawParam::default().scale(scale));
+        }
         canvas.finish(ctx)?;
+
+
         Ok(())
     }
 }
