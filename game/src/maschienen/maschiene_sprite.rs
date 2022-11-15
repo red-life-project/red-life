25d96515ee9ebae1604c9ee4d6d5e2bcb2da7b3f
sander <sander.stella00@gmail.com>
2022-11-15 20:52:37 +0100

    re added code that was missing after merge

--- game/src/maschienen/maschiene_sprite.rs
+++ game/src/maschienen/maschiene_sprite.rs
@@ -1,1 +1,32 @@
+use ggez::graphics::{Image};
+
+pub struct MaschineSprite {
+    name:String,
+    idel:Image,
+    broken: Image,
+    running: Image,
+}
+
+impl Default for MaschineSprite{
+    fn default() -> Self {
+        Self{
+            name:  String::default("Machiene ohne namen"),
+            idel: Image(),
+            broken: Image(),
+            running: Image()
+        }
+    }
+}
+
+
+impl MaschineSprite {
+
+    pub fn name(&self) -> String {
+        self.name
+    }
+    pub fn new(name: String, idel: Image, broken: Image, running: Image) -> Self {
+        Self { name, idel, broken, running }
+    }
+
+}
 
--- game/src/maschienen/trade.rs
+++ game/src/maschienen/trade.rs
@@ -1,1 +1,23 @@
+use std::collections::HashMap;
+use std::path::Iter;
+use ggez::glam::Vec2;
+use super::super::game_core::item;
 
+struct Trade {
+    time_ms: usize,
+    cost: HashMap<item, usize>,
+    result: item,
+    amount_produced: usize,
+}
+
+impl Default for Trade {
+    fn default() -> Self {
+
+        Self{
+            time_ms:1000,
+            cost:HashMap::default(),
+            result:item,
+            amount_produced: 0
+        }
+    }
+}
