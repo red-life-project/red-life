f804281bcf7eadff0933ac6e14c5d1fce173e5fc
mfloto <60036186+mfloto@users.noreply.github.com>
2022-11-16 22:33:54 +0100

    - Scale button-rect in is_clicked() methode - Move draw_button() to buttons.rs

--- game/src/main_menu/button.rs
+++ game/src/main_menu/button.rs
@@ -1,8 +1,9 @@
 use crate::main_menu::mainmenu::Message;
-use ggez::graphics;
 use ggez::graphics::Color;
 use ggez::mint::Point2;
+use ggez::{graphics, Context, GameResult};
 use std::sync::mpsc::Sender;
+use ggez::glam::f32::Vec2;
 
 #[derive(Debug)]
 pub(crate) struct Button {
@@ -19,13 +20,23 @@ impl Button {
         dbg!("Pressed {:?}", self.message);
     }
 
-    fn is_clicked(&self, mouse_pos: Point2<f32>) -> bool {
-        self.rect.contains(mouse_pos)
+    fn is_clicked(&self, mouse_pos: Point2<f32>, scale: &Vec2) -> bool {
+        let button_rect = self.clone();
+        button_rect.scale(scale.x, scale.y).contains(mouse_pos)
+        //self.rect.contains(mouse_pos)
     }
     pub(crate) fn click(&mut self, mouse_pos: Point2<f32>) {
-        if self.is_clicked(mouse_pos) {
+        if self.is_clicked(mouse_pos, &Default::default()) {
             self.pressed();
             self.sender.send(self.message).unwrap();
         }
     }
+}
+
+pub fn draw_button(ctx: &mut Context, btn: &Button) -> GameResult<graphics::Mesh> {
+    let mb = &mut graphics::MeshBuilder::new();
+
+    mb.rectangle(graphics::DrawMode::fill(), btn.rect, btn.color)?;
+
+    Ok(graphics::Mesh::from_data(ctx, mb.build()))
 }
