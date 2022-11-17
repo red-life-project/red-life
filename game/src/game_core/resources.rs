a0e6d999fb1a57b7c44218d6a89f7e62c9dacb09
Marion Hinkel <marion@fauxia.de>
2022-11-17 09:37:27 +0100

    added death to game and changed loction of player and Item struct into the files

--- game/src/game_core/resources.rs
+++ game/src/game_core/resources.rs
@@ -46,9 +46,9 @@ impl Resources<u16> {
     // This function returns the value that reached zero first
     // If no value reached zero, it returns None
     pub fn get_zero_values(&self) -> Option<DeathReason> {
-        if self.oxygen <= 0 {
+        if self.oxygen == 0 {
             Some(DeathReason::Oxygen)
-        } else if self.energy <= 0 {
+        } else if self.energy == 0 {
             Some(DeathReason::Energy)
         } else {
             None
