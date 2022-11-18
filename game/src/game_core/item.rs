26e4afe8beb08ba6cd657caef6117b38becbc767
Marion Hinkel <marion@fauxia.de>
2022-11-18 16:11:30 +0100

    added function to item and amount

--- game/src/game_core/item.rs
+++ game/src/game_core/item.rs
@@ -1,4 +1,3 @@
-use ggez::graphics::{Image, Rect};
 use serde::{Deserialize, Serialize};
 /// Defines an item in the inventory of the player
 /// Contains the name of the item, information about the item and the image
@@ -6,6 +5,13 @@ use serde::{Deserialize, Serialize};
 pub(crate) struct Item {
     name: String,
     info_text: String,
+    position: (usize, usize),
     //image should be a texture, didnt work yet
     img: String,
+    amount: i16,
+}
+impl Item {
+    pub fn change_amount(&mut self, amount: i16) {
+        self.amount += amount;
+    }
 }
