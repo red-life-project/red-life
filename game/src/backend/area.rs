ec3fd35e2bf2256c4efe96ca743da4576b363c90
Marion Hinkel <91947236+marionhinkel@users.noreply.github.com>
2022-11-19 17:30:51 +0100

    Formatting, inlining

--- game/src/backend/area.rs
+++ game/src/backend/area.rs
@@ -1,10 +1,15 @@
+use crate::backend::utils::is_colliding;
 use crate::game_core::player::Player;
 use dyn_clone::DynClone;
+use ggez::glam::Vec2;
 use ggez::graphics::Rect;
 use std::fmt::Debug;
 
 pub trait Area: DynClone + Debug {
     fn interact(&mut self, player: &Player);
+    fn is_interactable(&self, pos: (usize, usize)) -> bool {
+        is_colliding(pos, &self.get_interaction_area())
+    }
     fn get_collision_area(&self) -> Rect;
     fn get_interaction_area(&self) -> Rect;
 }
