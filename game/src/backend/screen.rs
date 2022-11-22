a44ca566b8746b8c0ee0bb4698e5b878979eba0c
Flippchen <91947480+flippchen@users.noreply.github.com>
2022-11-22 23:17:52 +0100

    - first impl of resized Popups with borders

--- game/src/backend/screen.rs
+++ game/src/backend/screen.rs
@@ -1,12 +1,14 @@
 use crate::backend::utils::get_scale;
 use crate::error::RLError;
 use crate::main_menu::mainmenu::MainMenu;
-use crate::RLResult;
-use ggez::graphics::Color;
+use crate::{draw, RLResult};
+use ggez::graphics::{Color, Text};
 use ggez::{event, graphics, Context};
 use std::fmt::Debug;
 use std::sync::mpsc::{channel, Receiver, Sender};
 use std::time::Instant;
+use ggez::conf::FullscreenType::True;
+use ggez::glam::vec2;
 
 /// A screen is every drawable object in the game, so the main menu is a screen too
 pub trait Screen: Debug {
@@ -56,7 +58,25 @@ impl Screenstack {
         let mut canvas = graphics::Canvas::from_frame(ctx, None);
         for (pos, popup) in self.popup.iter().enumerate() {
             let scale = get_scale(ctx);
-            let text = graphics::Text::new(popup.text.clone());
+            let mut text = graphics::Text::new(popup.text.clone());
+            text.set_scale(18.);
+            let dimensions = text.measure(ctx)?;
+            let x = dimensions.x;
+            let y = dimensions.y;
+            let rect = graphics::Mesh::new_rectangle(
+                ctx,
+                graphics::DrawMode::fill(),
+                graphics::Rect::new(0., 0., x + 2., y + 2.),
+                Color::from_rgb(148, 147, 143),
+            )?;
+            let outer = graphics::Mesh::new_rectangle(
+                ctx,
+                graphics::DrawMode::stroke(3.),
+                graphics::Rect::new(0., 0., x + 3., y + 3.),
+                Color::from_rgb(24, 26, 25),
+            )?;
+            draw!(canvas, &rect,vec2(0., pos as f32 * 100.), scale);
+            draw!(canvas, &outer,vec2(0., pos as f32 * 100.), scale);
             canvas.draw(
                 &text,
                 graphics::DrawParam::default()
