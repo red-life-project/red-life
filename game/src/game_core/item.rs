d40b06835becd5490b31368cd69706572452f786
Marion Hinkel <marion@fauxia.de>
2022-11-24 10:55:40 +0100

    added draw item function

--- game/src/game_core/item.rs
+++ game/src/game_core/item.rs
@@ -20,7 +20,7 @@ pub const BENZIN: [&str; 3] = [
 pub struct Item {
     pub name: String,
     info_text: String,
-    img: String,
+    pub img: String,
 }
 impl Item {
     pub(crate) fn new(item: [&str; 3]) -> Self {
