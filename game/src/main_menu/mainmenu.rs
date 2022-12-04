c85a730f4e966214878701ad9b2356e546b04664
Flippchen <91947480+Flippchen@users.noreply.github.com>
2022-12-04 16:14:48 +0100

    temp fix for memory

--- game/src/main_menu/mainmenu.rs
+++ game/src/main_menu/mainmenu.rs
@@ -27,6 +27,8 @@ pub struct MainMenu {
     receiver: Receiver<Message>,
     sender: Sender<Message>,
     screen_sender: Sender<StackCommand>,
+    background: &'static [u8; 121631],
+    background_image: Option<graphics::Image>,
 }
 
 impl MainMenu {
@@ -65,6 +67,8 @@ impl MainMenu {
             receiver,
             sender,
             screen_sender,
+            background: include_bytes!("../../../assets/mainmenu.png"),
+            background_image: None,
         }
     }
     //@rewierer ich würde diese funktion später entfernen da ich sie aktuel noch nutzen mag
@@ -90,7 +94,9 @@ impl Screen for MainMenu {
         self.buttons.iter_mut().for_each(|btn| {
             btn.action(ctx, scale);
         });
-
+        if self.background_image.is_none() {
+            self.background_image = Some(graphics::Image::from_bytes(ctx, self.background)?);
+        }
         if let Ok(msg) = self.receiver.try_recv() {
             match msg {
                 Exit => std::process::exit(0),
@@ -114,10 +120,9 @@ impl Screen for MainMenu {
     fn draw(&self, ctx: &mut Context) -> RLResult {
         let scale = get_scale(ctx);
         let mut canvas = graphics::Canvas::from_frame(ctx, RLColor::DARK_BLUE);
-        let background =
-            graphics::Image::from_bytes(ctx, include_bytes!("../../../assets/mainmenu.png"))?;
-        canvas.draw(&background, graphics::DrawParam::default().scale(scale));
-
+        if let Some(background) = &self.background_image {
+            canvas.draw(background, graphics::DrawParam::default().scale(scale));
+        }
         for btn in &self.buttons {
             btn.draw_button(ctx, &mut canvas)?;
         }
